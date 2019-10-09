d5ac09bfb11d0997418666b3646c94f76b833b49
Qs-F <qsf@de-liker.com>
2019-10-09 10:39:37 +0900

    Fix CORS

--- lserver.go
+++ lserver.go
@@ -58,7 +58,7 @@ func (o *Option) newServer() *Server {
 
 func cors(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		h.ServeHTTP(w, r)
 	})
 }
